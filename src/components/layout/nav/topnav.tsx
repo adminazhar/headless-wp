02a7fe4e7297141ad3c89ba447f97dec4b1fe57f
adminazhar <admin@azhark.com>
2023-09-03 02:43:51 +0530

    add topnav

---
+++ src/components/layout/nav/topnav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TopNav = () => {
+  return (
+    <ul>
+    <li>Articles</li>
+    <li>Categories</li>
+    <li>Tags</li>
+    <li>Single Article</li>
+    </ul>
+  )
+}
