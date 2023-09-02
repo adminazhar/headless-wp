24284162cc3a89e91226ca66fdd3ad25002a9145
adminazhar <admin@azhark.com>
2023-09-03 02:44:04 +0530

    add headerlayout

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TopNav } from '../nav/topnav'
+export const HeaderLayout = () => {
+  return (
+    <div>
+        <div>HeaderLayout11</div>
+        <TopNav></TopNav>
+    </div>
+  )
+}
