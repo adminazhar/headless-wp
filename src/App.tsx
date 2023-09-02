4cf1549b39cf84e2a81ec1342000e4569a0a183d
adminazhar <admin@azhark.com>
2023-09-03 02:44:46 +0530

    update App components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { HeaderLayout } from './components/layout/header/header';
+import { FooterLayout } from './components/layout/footer/footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Yo WP!</p>
-      </header>
+    <div>
+      <HeaderLayout></HeaderLayout>
+      <p>Yo WP!</p>
+      <FooterLayout></FooterLayout>
     </div>
   );
 }
